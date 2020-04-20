93b0c8601b576423ec3be45ebb53e465d22c6989
dietlfab <fabian.dietl@sws.de>
2020-04-20 13:47:44 +0200

    Aufgabe 1 erledigt und umbenannt

---
+++ aufgabe 1.py
@@ -0,0 +1,9 @@
+number = 6
+# Schreibe die Berechnung in folgende Zeile
+result = 0
+
+result = (number*2+10)/2
+
+print (int(result))
+
+print ("Du hast %d ausgewählt, das Ergebnis ist %d!" % (number, result))
